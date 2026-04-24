2161469169244c5e112395f8be2bbdacbd6acdd2
ibraum <ibraumodnok@gmail.com>
2026-04-24 23:08:44 +0000

    Update

--- src/components/organisms/home/Hero.tsx
+++ src/components/organisms/home/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <div className="max-2xl:text-[38px] 2xl:text-[56px] text-cocoa font-medium">Every cocoa bean has a story.
             &nbsp; <span className="text-cocoa-60">We make</span> it visible.</div>
           <p className="max-2xl:text-[18px] max-2xl:leading-[28px] 2xl:text-[28px] text-cocoa font-normal">From remote farms to global markets, Kafara captures every step of the journey, turning invisible processes into trusted, verifiable data.</p>
-          <div className="flex gap-4 max-2xl:h-11 2xl:h-16 max-2xl:max-w-44.75 2xl:max-w-58 max-2xl:rounded-r6 2xl:rounded-r10 pl-5.5 pr-1.5 py-1 items-center justify-between bg-cocoa w-full text-white max-2xl:text-[14px] 2xl:text-[18px] font-medium">
+          <div className="flex gap-4 max-2xl:h-11 2xl:h-16 max-2xl:max-w-44.75 2xl:max-w-58 max-2xl:rounded-r6 2xl:rounded-r12 pl-5.5 pr-1.5 py-1 items-center justify-between bg-cocoa w-full text-white max-2xl:text-[14px] 2xl:text-[18px] font-medium">
             <div className="">Get Started</div>
             <div className="w-8 h-8 rounded-full bg-white"></div>
           </div>
