698be51036e7c2e928d27453a24f21135e8fcb85
ibraum <ibraumodnok@gmail.com>
2026-04-25 01:03:36 +0000

    jgjf

--- src/components/organisms/home/BuildFor.tsx
+++ src/components/organisms/home/BuildFor.tsx
@@ -1,7 +1,26 @@
 const BuildFor = () => {
   return (
-    <div>
-      
+    <div className="w-full h-screen flex flex-col items-center justify-center relative p-16 gap-10">
+      <img src="./logo-tiny.png" alt="" />
+      <div className="max-w-[740px] text-[48px] font-medium leading-[54px] text-cocoa text-center">
+        Built for every actor in the cocoa ecosystem
+      </div>
+      <div className="h-[170px] w-[194px] border border-2 rounded-r7 p-2 gap-4 absolute bottom-[15%] left-[25%] rotate-[10.89deg]">
+        <img src="./c1.png" alt="" />
+        <div className="text-[24px] w-full text-center text-normal leading-[28px] ">Cooperatives</div>
+      </div>
+      <div className="h-[170px] w-[194px] border border-2 rounded-r7 p-2 gap-4 absolute bottom-[20%] right-[30%] rotate-[-5.65deg]">
+        <img src="./c2.png" alt="" />
+        <div className="text-[24px] w-full text-center text-normal leading-[28px]">Exporters</div>
+      </div>
+      <div className="h-[170px] w-[194px] border border-2 rounded-r7 p-2 gap-4 absolute top-[30%] left-[20%] rotate-[-21.9deg]">
+        <img src="./c3.png" alt="" />
+        <div className="text-[24px] w-full text-center text-normal leading-[28px]">Producers</div>
+      </div>
+      <div className="h-[170px] w-[194px] border border-2 rounded-r7 p-2 gap-4 absolute top-[15%] right-[25%] rotate-[20.12deg]">
+        <img src="./c4.png" alt="" />
+        <div className="text-[24px] w-full text-center text-normal leading-[28px]">Regulators</div>
+      </div>
     </div>
   )
 }
--- src/components/organisms/home/HowItWork.tsx
+++ src/components/organisms/home/HowItWork.tsx
@@ -1,7 +1,39 @@
 const HowItWork = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-start gap-16 h-screen p-16">
+      <div className="max-w-[364px] text-[32px] leading-[42px] 2xl:max-w-[420px] 2xl:text-[36px] 2xl:leading-[46px] font-medium  text-cocoa">From farm to export, fully traceable</div>
+      <div className="flex flex-col gap-5 w-full">
+        <div className="w-full h-[56px] 2xl:h-[15vh] grid grid-cols-5">
+          <div className="w-full h-[56px] 2xl:h-[15vh] border border-2 border-cocoa col-span-2 rounded-r9 pr-20 py-2 pl-2 flex items-center justify-start gap-4">
+            <div className="h-full"><img src="./trace1.png" className="h-full" alt="" /></div>
+            <div className="text-[#000] text-[26px] leading-[42px]">Map farms with GPS data</div>
+          </div>
+        </div>  
+        <div className="w-full h-[56px] 2xl:h-[15vh] grid grid-cols-5">
+          <div className=""></div>
+          <div className="w-full h-[56px] 2xl:h-[15vh] border border-2 border-cocoa col-span-2 rounded-r9 pr-20 py-2 pl-2 flex items-center justify-start gap-4">
+            <div className="h-full"><img src="./trace2.png" className="h-full" alt="" /></div>
+            <div className="text-[#000] text-[26px] leading-[42px]">Collect and digitize field information</div>
+          </div>
+        </div>  
+        <div className="w-full h-[56px] 2xl:h-[15vh] grid grid-cols-5">
+          <div className=""></div>
+          <div className=""></div>
+          <div className="w-full h-[56px] 2xl:h-[15vh] border border-2 border-cocoa col-span-2 rounded-r9 pr-20 py-2 pl-2 flex items-center justify-start gap-4">
+            <div className="h-full shrink-0"><img src="./trace3.png" className="h-full shrink-0" alt="" /></div>
+            <div className="text-[#000] text-[26px] leading-[42px]">Track product movement across the supply chain</div>
+          </div>
+        </div>  
+        <div className="w-full h-[56px] 2xl:h-[15vh] grid grid-cols-5">
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+          <div className="w-full h-[56px] 2xl:h-[15vh] border border-2 border-cocoa col-span-2 rounded-r9 pr-20 py-2 pl-2 flex items-center justify-start gap-4">
+            <div className="h-full shrink-0"><img src="./trace4.png" className="h-full" alt="" /></div>
+            <div className="text-[#000] text-[26px] leading-[42px]">Secure data for verification and compliance</div>
+          </div>
+        </div>  
+      </div>
     </div>
   )
 }
