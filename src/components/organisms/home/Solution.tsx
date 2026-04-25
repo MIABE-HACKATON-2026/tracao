65b8edde84dcd8e9b75a414fb78f01a107d56e9b
ibraum <ibraumodnok@gmail.com>
2026-04-25 00:30:25 +0000

    jgjf

--- src/components/organisms/home/Solution.tsx
+++ src/components/organisms/home/Solution.tsx
@@ -1,7 +1,11 @@
 const Solution = () => {
   return (
-    <div>
-      
+    <div className="w-full h-screen flex flex-col items-center justify-center p-16">
+      <div className="w-full h-full flex flex-col items-center justify-center gap-10">
+        <img src="./logo-own.png" alt="" />
+        <div className="max-w-[612px] text-[48px] leading-[64px] font-medium text-cocoa text-center">Bridging local production and global compliance</div>
+        <div className="max-w-[740px] text-[28px] leading-[40px] font-light text-cocoa text-center">Tracao digitizes the cocoa supply chain by capturing field data, tracking movements, and securing records using modern technologies.</div>
+      </div>
     </div>
   )
 }
